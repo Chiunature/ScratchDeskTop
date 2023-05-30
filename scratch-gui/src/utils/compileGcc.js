889c026bd95753f3533efca274a5285b35520ad3
jiang <2367269651@qq.com>
2023-05-30 10:10:34 +0800

    优化编译代码

--- scratch-gui/src/utils/compileGcc.js
+++ scratch-gui/src/utils/compileGcc.js
@@ -1,3 +1,10 @@
+/*
+ * @Description: New features
+ * @Author: jiang
+ * @Date: 2023-05-29 14:28:01
+ * @LastEditors: jiang
+ * @LastEditTime: 2023-05-30 10:10:06
+ */
 const fs = window.fs;
 const path = window.path;
 const process = window.child_process;
@@ -9,47 +16,47 @@ function writeFile(buffer, callback) {
         if (err) return callback(err);
         let file = `./codes/cake/` + `test${Date.now().toString(36)}.c`;
         fs.writeFile(file, buffer, function (err) {
-            return callback(file, err);
+            return callback(err, file);
         });
+    });
+}
+
+function processCMD(commend, callback) {
+    process.exec(commend, (error, stdout, stderr) => {
+        if (error) return callback(error);
+        return callback(error, stdout);
     });
 }
 
 function compile(file) {
     let ph = path.resolve(__dirname, "/scratch/scratch-gui/mingw64/bin");
     //先获取环境变量看看有没有设置
-    process.exec(`set path`, (error, stdout, stderr) => {
+    processCMD(`set path`, (error, stdout) => {
+        if (error) return;
         let res = stdout.slice(5).split(";");
         let p = ph.replace(/\//g, "\\\\");
+        let gcc = `gcc -o ${file.replace(/\.+c/g, ".bin")} ${file}`;
         //如果设置了就跳过直接执行编译，没有就设置之后再执行编译
         if (res.includes(p)) {
-            process.exec(
-                `gcc -o ${file.replace(/\.+c/g, ".bin")} ${file}`,
-                (error, stdout, stderr) => {
-                    console.log(error, stdout, stderr);
-                }
-            );
+            processCMD(gcc, (error, stdout) => {
+                console.log("设置了环境变量跳过直接执行编译=>", error, stdout);
+            });
             return;
         } else {
-            process.exec(
-                `setx "path" "${ph};%path%"`,
-                (error, stdout, stderr) => {
-                    if (error) return;
-                    process.exec(
-                        `gcc -o ${file.replace(/\.+c/g, ".bin")} ${file}`,
-                        (error, stdout, stderr) => {
-                            console.log(error, stdout, stderr);
-                        }
-                    );
-                }
-            );
+            processCMD(`setx "path" "${ph};%path%"`, (error, stdout) => {
+                if (error) return;
+                processCMD(gcc, (error, stdout) => {
+                    console.log(error, stdout);
+                });
+            });
         }
     });
 }
 
 //运行编译器参数是传入的C语言代码
 function runGcc(buffer = str) {
-    writeFile(buffer, (file, err) => {
-        if (err) console.error(err);
+    writeFile(buffer, (err, file) => {
+        if (err) return;
         console.info("write success");
         if (file) compile(file);
     });
