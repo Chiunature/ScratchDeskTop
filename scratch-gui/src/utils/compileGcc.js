6443a47816a28fc6257a9e95081f9d922fa8b8c0
jiang <2367269651@qq.com>
2023-07-13 18:26:31 +0800

    修改编译

--- scratch-gui/src/utils/compileGcc.js
+++ scratch-gui/src/utils/compileGcc.js
@@ -3,14 +3,13 @@ import { handlerError } from "./ipcRender";
 const fs = window.fs;
 const path = window.path;
 const process = window.child_process;
-
 const makeCommand = 'make';
-const makefile = './myLED';
+const makefile = './LB_USER';
 const cmd = `cd ./gcc-arm-none-eabi/bin&&${makeCommand} -C ${makefile}`;
 
 //将c语言代码写入文件
 function writeFiles(buffer, callback) {
-    let file = path.join(`./gcc-arm-none-eabi/bin/myLED/USER/`, 'main.c');
+    let file = path.join(`./gcc-arm-none-eabi/bin/LB_USER/Src`, 'Aplication.c');
     fs.writeFile(file, buffer, (err) => {
         return callback(err);
     });
@@ -29,7 +28,7 @@ function compile() {
     processCMD(cmd, (stdout) => {
         console.log(`stdout: ${stdout}`);
         // getFiles(path.join(ph, file.replace(/\.+c/g, ".bin")));
-        getFiles("./gcc-arm-none-eabi/bin/myLED/build/myLED.bin");
+        getFiles("./gcc-arm-none-eabi/bin/LB_USER/build/LB_USER.bin");
     });
 }
 
@@ -37,7 +36,7 @@ function compile() {
 function runGcc(buffer) {
     const pattern = /(int main\s*\(\s*void\s*\)|int main\s*\(\s*\))\s*{([\s\S]+)\}/g;
     let match = pattern.exec(buffer);
-    let code = `#include "stm32f10x.h"\n#include "stdio.h"\n#include "stm32f10x_gpio.h"\n#include "delay.h"\n#include "stm32f10x_usart.h"\n#define LED_Troger (*(void (*)())0x8030001)\n#define Delay_500ms (*(void (*)())0x8030081)\n#define printf_usart (*(void (*)(char *))0x8030161)\nint main(void) {\n SCB->VTOR = FLASH_BASE | 0x20000;\nDelay_500ms();\nDelay_500ms();\nprintf_usart(" jump to App1 OK!");\n${match[2]}\nwhile (1){\n LED_Troger();\n}\n}`;
+    let code = `#include "main.h"\nvoid USER_Aplication(void)\n{\n${match[2]}\n}`;
     writeFiles(code, (err) => {
         if (err) return handlerError(err);
         console.info("write success");
