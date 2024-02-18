fe50fc36dc803f1b08cb53f9fb106ade934467af
jiang <2367269651@qq.com>
2024-02-18 10:55:41 +0800

    暂时隐藏矩阵灯修改颜色饱和度积木

--- scratch-gui/src/lib/make-toolbox-xml.js
+++ scratch-gui/src/lib/make-toolbox-xml.js
@@ -609,6 +609,14 @@ const combined_motor = function (isInitialSetup, isStage, targetId, colors) {
     `;
 };
 
+/* 
+<block type="matrix_lamp_setSaturation">
+            <value name="saturation">
+                <shadow type="math_0to100_number"><field name="NUM">50</field></shadow>
+            </value>
+        </block>
+ */
+
 const matrix = function (isInitialSetup, isStage, targetId, colors) {
     const stageSelected = ScratchBlocks.ScratchMsgs.translate(
         "MOTOR_STAGE_SELECTED",
@@ -633,11 +641,6 @@ const matrix = function (isInitialSetup, isStage, targetId, colors) {
         <block type="matrix_lamp_stop"></block>
         <block type="matrix_lamp_set">
             <value name="brightness">
-                <shadow type="math_0to100_number"><field name="NUM">50</field></shadow>
-            </value>
-        </block>
-        <block type="matrix_lamp_setSaturation">
-            <value name="saturation">
                 <shadow type="math_0to100_number"><field name="NUM">50</field></shadow>
             </value>
         </block>
