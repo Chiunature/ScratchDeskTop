823c80ddcefd967f1f49f4806e45c25279967b83
jiang <2367269651@qq.com>
2024-07-17 17:39:21 +0800

    添加Blockly.StaticTyping

--- scratch-blocks/core/blockly.js
+++ scratch-blocks/core/blockly.js
@@ -60,6 +60,7 @@ goog.require('Blockly.Generator');
 goog.require('Blockly.Msg');
 goog.require('Blockly.Procedures');
 goog.require('Blockly.ScratchMsgs');
+goog.require('Blockly.StaticTyping');
 goog.require('Blockly.Toolbox');
 goog.require('Blockly.Touch');
 goog.require('Blockly.WidgetDiv');
