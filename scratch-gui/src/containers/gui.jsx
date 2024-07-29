ae7bc80c05728f6d53e5bd395e75ed49f580c93a
jiang <2367269651@qq.com>
2024-07-29 16:42:52 +0800

    硬件改成209

--- scratch-gui/src/containers/gui.jsx
+++ scratch-gui/src/containers/gui.jsx
@@ -50,7 +50,7 @@ import TipsForUpdate from "../components/alerts/tipsForUpdate.jsx";
 import getMainMsg from "../lib/alerts/message.js";
 import debounce from "lodash.debounce";
 
-const FIRMWARE_VERSION = '208';
+const FIRMWARE_VERSION = '209';
 class GUI extends React.Component {
     constructor(props) {
         super(props);
