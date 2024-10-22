c319d2a61dd424d3f2e2d42a2435cb3e99e52b99
Rales <87068287+njkndxz@users.noreply.github.com>
2024-10-22 10:06:09 +0800

    程序列表没有时显示空状态

--- scratch-gui/src/components/device/selectExe.jsx
+++ scratch-gui/src/components/device/selectExe.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 import styles from './device.css';
 import delImg from './delete.svg';
 import downloadImg from './download.svg';
-
+import emptyIcon from "../project-management/icon--empty.svg";
 
 const SelectExe = (props) => {
     const { exeList, completed, selectedExe, handleSelectExe, onSetExelist, handleCompile, handleDelExe } = props;
@@ -35,22 +35,29 @@ const SelectExe = (props) => {
 
 
     return (
-        <ul className={styles.selExe} ref={refUl}>
-            {exeList.map((item, index) => {
-                return (
-                    <li className={completed ? styles.ban : ''} key={item.num} draggable onClick={() => handleSelectExe(item, index)}>
-                        <div><input type='radio' checked={item.checked} readOnly />{item.num}</div>
-                        <div className={styles.edit}>
-                            <input className={item.checked ? styles.check : ''} type='text' disabled={!item.checked} ref={(c) => refObj.current[index] = c} defaultValue={item.name} onChange={handleInpChange} />
-                        </div>
-                        <div className={styles.img}>
-                            <img src={downloadImg} onClick={() => download(item)} />
-                            <img src={delImg} onClick={(e) => handleDelExe(item, e)} />
-                        </div>
-                    </li>
-                )
-            })}
-        </ul>
+        <>
+            {
+                exeList.length > 0 ?
+                    (<ul className={styles.selExe} ref={refUl}>
+                        {
+                            exeList.map((item, index) => {
+                                return (
+                                    <li className={completed ? styles.ban : ''} key={item.num} draggable onClick={() => handleSelectExe(item, index)}>
+                                        <div><input type='radio' checked={item.checked} readOnly />{item.num}</div>
+                                        <div className={styles.edit}>
+                                            <input className={item.checked ? styles.check : ''} type='text' disabled={!item.checked} ref={(c) => refObj.current[index] = c} defaultValue={item.name} onChange={handleInpChange} />
+                                        </div>
+                                        <div className={styles.img}>
+                                            <img src={downloadImg} onClick={() => download(item)} alt='' />
+                                            <img src={delImg} onClick={(e) => handleDelExe(item, e)} alt='' />
+                                        </div>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>) : (<img style={{ margin: 20 }} width={200} src={emptyIcon} alt='' />)
+            }
+        </>
     );
 }
 
