7294275e07b8fd34333ef53a43a76d62f3a7e78b
Rales <87068287+njkndxz@users.noreply.github.com>
2025-03-04 10:22:40 +0800

    修改摇杆积木参数，版本更新至184

--- scratch-blocks/generators/python/sensing.js
+++ scratch-blocks/generators/python/sensing.js
@@ -181,7 +181,7 @@ Blockly.Python['sensing_Handling'] = function (block) {
     const keys = block.getFieldValue('KEYS');
     const button = block.getFieldValue('BUTTON');
     // TODO: Assemble Python into code variable.
-    const code = Blockly.Python.handleResult(`Handling("${button}", "${keys}")`, Blockly.Python.KEY_TYPE);
+    const code = Blockly.Python.handleResult(`Handling("${keys}", "${button}")`, Blockly.Python.KEY_TYPE);
     return [code.trim(), Blockly.Python.ORDER_ATOMIC];
 };
 
