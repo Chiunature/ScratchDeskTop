22d77495bf7829b564512f303c6591b11647dee1
Chiunature <1540090646@qq.com>
2025-10-28 16:17:39 +0800

    fix角度和速度位置

--- scratch-gui/src/components/device/deviceBox.jsx
+++ scratch-gui/src/components/device/deviceBox.jsx
@@ -1,9 +1,7 @@
-import React, { Fragment, useMemo } from 'react';
-import styles from './device.css';
-
+import React, { Fragment, useMemo } from "react";
+import styles from "./device.css";
 
 const DeviceBox = ({ list, intl, messages }) => {
-
     let newList = useMemo(() => list, [list]);
 
     function getPort(index) {
@@ -11,21 +9,21 @@ const DeviceBox = ({ list, intl, messages }) => {
         if (isNaN(num)) return;
         switch (num) {
             case 0:
-                return 'A';
+                return "A";
             case 1:
-                return 'B';
+                return "B";
             case 2:
-                return 'C';
+                return "C";
             case 3:
-                return 'D';
+                return "D";
             case 4:
-                return 'E';
+                return "E";
             case 5:
-                return 'F';
+                return "F";
             case 6:
-                return 'G';
+                return "G";
             case 7:
-                return 'H';
+                return "H";
             default:
                 break;
         }
@@ -34,13 +32,13 @@ const DeviceBox = ({ list, intl, messages }) => {
     function motorData(num) {
         switch (num) {
             case 0:
-                return intl.formatMessage(messages['circly']);
+                return intl.formatMessage(messages["circly"]);
             case 1:
-                return intl.formatMessage(messages['angle']);
+                return intl.formatMessage(messages["actualSpeed"]);
             case 2:
-                return intl.formatMessage(messages['actualSpeed']);
+                return intl.formatMessage(messages["angle"]);
             case 3:
-                return intl.formatMessage(messages['version']);
+                return intl.formatMessage(messages["version"]);
             default:
                 return;
         }
@@ -48,21 +46,21 @@ const DeviceBox = ({ list, intl, messages }) => {
 
     function colorData(keyName) {
         switch (keyName) {
-            case 'l':
-                return intl.formatMessage(messages['lightIntensity']);
-            case 'rgb':
-                return 'RGB';
-            case 'version':
-                return intl.formatMessage(messages['version']);
+            case "l":
+                return intl.formatMessage(messages["lightIntensity"]);
+            case "rgb":
+                return "RGB";
+            case "version":
+                return intl.formatMessage(messages["version"]);
             default:
-                return 'Error';
+                return "Error";
         }
     }
 
     function grayData(keyName) {
         switch (keyName) {
-            case 'version':
-                return intl.formatMessage(messages['version']);
+            case "version":
+                return intl.formatMessage(messages["version"]);
             default:
                 return keyName;
         }
@@ -73,55 +71,114 @@ const DeviceBox = ({ list, intl, messages }) => {
             {newList.map((el, index) => {
                 return (
                     <div className={styles.midBox} key={index}>
-                        <p>{intl.formatMessage(messages['port'])}: {getPort(el.port)}-{intl.formatMessage(messages[el.sensing_device])}</p>
-                        {el.motor && Object.keys(el.motor).length > 0 && <ul className={styles.midUl}>
-                            {Object.keys(el.motor).map((keyName, index) => {
-                                return (
-                                    <Fragment key={index}>
-                                        {
-                                            motorData(index) &&
-                                            <li>
-                                                <span>{motorData(index)}</span>
-                                                <span>{el.motor[keyName]}</span>
-                                            </li>
-                                        }
-                                    </Fragment>
-                                )
-                            })}
-                        </ul>}
-                        {el.ultrasion && <ul className={styles.midUl}><li><span>{intl.formatMessage(messages['distance'])}</span><span>{el.ultrasion.cm}</span><span>cm</span></li></ul>}
-                        {el.touch && <ul className={styles.midUl}><li><span>{intl.formatMessage(messages['key'])}</span><span>{el.touch.state}</span></li></ul>}
-                        {el.color && Object.keys(el.color).length > 0 && <ul className={styles.midUl}>
-                            {Object.keys(el.color).map((keyName, index) => {
-                                return (
-                                    <Fragment key={index}>
-                                        {
-                                            (keyName === 'l' || keyName === 'rgb' || keyName === 'version' || keyName === 'Not_Run') &&
-                                            <li>
-                                                <span>{colorData(keyName)}</span>
-                                                <span>{el.color[keyName]}</span>
-                                                {keyName === 'rgb' && <span><div className={styles.col} style={{ 'backgroundColor': el.color[keyName] }}></div></span>}
-                                            </li>
-                                        }
-                                    </Fragment>
-                                )
-                            })}
-                        </ul>}
-                        {el.gray && Object.keys(el.gray).length > 0 && <ul className={styles.midUl}>
-                            {Object.keys(el.gray).map((keyName, index) => {
-                                return (
-                                    <li key={index}>
-                                        <span>{keyName === 'Not_Run' ? 'Error' : grayData(keyName)}</span>
-                                        <span>{el.gray[keyName]}</span>
-                                    </li>
-                                )
-                            })}
-                        </ul>}
+                        <p>
+                            {intl.formatMessage(messages["port"])}:{" "}
+                            {getPort(el.port)}-
+                            {intl.formatMessage(messages[el.sensing_device])}
+                        </p>
+                        {el.motor && Object.keys(el.motor).length > 0 && (
+                            <ul className={styles.midUl}>
+                                {Object.keys(el.motor).map((keyName, index) => {
+                                    return (
+                                        <Fragment key={index}>
+                                            {motorData(index) && (
+                                                <li>
+                                                    <span>
+                                                        {motorData(index)}
+                                                    </span>
+                                                    <span>
+                                                        {el.motor[keyName]}
+                                                    </span>
+                                                </li>
+                                            )}
+                                        </Fragment>
+                                    );
+                                })}
+                            </ul>
+                        )}
+                        {el.ultrasion && (
+                            <ul className={styles.midUl}>
+                                <li>
+                                    <span>
+                                        {intl.formatMessage(
+                                            messages["distance"]
+                                        )}
+                                    </span>
+                                    <span>{el.ultrasion.cm}</span>
+                                    <span>cm</span>
+                                </li>
+                            </ul>
+                        )}
+                        {el.touch && (
+                            <ul className={styles.midUl}>
+                                <li>
+                                    <span>
+                                        {intl.formatMessage(messages["key"])}
+                                    </span>
+                                    <span>{el.touch.state}</span>
+                                </li>
+                            </ul>
+                        )}
+                        {el.color && Object.keys(el.color).length > 0 && (
+                            <ul className={styles.midUl}>
+                                {Object.keys(el.color).map((keyName, index) => {
+                                    return (
+                                        <Fragment key={index}>
+                                            {(keyName === "l" ||
+                                                keyName === "rgb" ||
+                                                keyName === "version" ||
+                                                keyName === "Not_Run") && (
+                                                <li>
+                                                    <span>
+                                                        {colorData(keyName)}
+                                                    </span>
+                                                    <span>
+                                                        {el.color[keyName]}
+                                                    </span>
+                                                    {keyName === "rgb" && (
+                                                        <span>
+                                                            <div
+                                                                className={
+                                                                    styles.col
+                                                                }
+                                                                style={{
+                                                                    backgroundColor:
+                                                                        el
+                                                                            .color[
+                                                                            keyName
+                                                                        ],
+                                                                }}
+                                                            ></div>
+                                                        </span>
+                                                    )}
+                                                </li>
+                                            )}
+                                        </Fragment>
+                                    );
+                                })}
+                            </ul>
+                        )}
+                        {el.gray && Object.keys(el.gray).length > 0 && (
+                            <ul className={styles.midUl}>
+                                {Object.keys(el.gray).map((keyName, index) => {
+                                    return (
+                                        <li key={index}>
+                                            <span>
+                                                {keyName === "Not_Run"
+                                                    ? "Error"
+                                                    : grayData(keyName)}
+                                            </span>
+                                            <span>{el.gray[keyName]}</span>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        )}
                     </div>
-                )
+                );
             })}
         </>
-    )
-}
+    );
+};
 
 export default DeviceBox;
