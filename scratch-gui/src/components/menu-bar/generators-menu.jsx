ac3ed705d8e4934b1e47ff68ec6c30a2a5df2dea
Chiunature <1540090646@qq.com>
2025-11-18 14:24:19 +0800

    python代码生成器UI取消下拉

--- scratch-gui/src/components/menu-bar/generators-menu.jsx
+++ scratch-gui/src/components/menu-bar/generators-menu.jsx
@@ -1,27 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import classNames from 'classnames';
-import menuBarStyles from './menu-bar.css';
-import styles from './settings-menu.css';
-import { FormattedMessage } from 'react-intl';
+import React, { useEffect, useState } from "react";
+import classNames from "classnames";
+import menuBarStyles from "./menu-bar.css";
+import styles from "./settings-menu.css";
+import { FormattedMessage } from "react-intl";
 import genIcon from "./icon--generator.svg";
-import dropdownCaret from './dropdown-caret.svg';
-import MenuBarMenu from './menu-bar-menu.jsx';
-import { MenuSection, MenuItem } from '../menu/menu.jsx';
+import dropdownCaret from "./dropdown-caret.svg";
+import MenuBarMenu from "./menu-bar-menu.jsx";
+import { MenuSection, MenuItem } from "../menu/menu.jsx";
 import check from "./check.svg";
 import ScratchBlocks from "scratch-blocks";
 import { genList } from "../../config/json/generators.json";
 
 function GeneratorsMenu(props) {
-    const { onSetGen, isGen, onClickGen, onRequestCloseGen, isRtl, genMenuOpen, onSetGeneratorName, generatorName, onGetCode, workspace } = props;
+    const {
+        onSetGen,
+        isGen,
+        onClickGen,
+        onRequestCloseGen,
+        isRtl,
+        genMenuOpen,
+        onSetGeneratorName,
+        generatorName,
+        onGetCode,
+        workspace,
+    } = props;
 
     const [typeName, setTypeName] = useState(generatorName);
 
     useEffect(() => {
-        window.myAPI.setStoreValue('generatorName', typeName);
-    }, [typeName])
+        window.myAPI.setStoreValue("generatorName", typeName);
+    }, [typeName]);
 
     function changeCodeName(item) {
-        const code = ScratchBlocks[item.generatorName].workspaceToCode(workspace);
+        const code =
+            ScratchBlocks[item.generatorName].workspaceToCode(workspace);
         onGetCode(code);
         setTypeName(item.typeName);
         if (generatorName !== item.generatorName) {
@@ -34,7 +46,6 @@ function GeneratorsMenu(props) {
         onSetGen(!isGen);
     }
 
-
     return (
         <div
             id="menuBarGen"
@@ -46,44 +57,29 @@ function GeneratorsMenu(props) {
                     [menuBarStyles.active]: genMenuOpen,
                 }
             )}
-            onMouseUp={onClickGen}
+            // onMouseUp={onClickGen}
+            onClick={() =>
+                changeCodeName({ generatorName: "Python", typeName: "Python" })
+            }
         >
-            <img className={menuBarStyles.unconnectedIcon} src={genIcon} alt="" />
-            <span className={menuBarStyles.collapsibleLabel} style={{ minWidth: '110px' }}>
+            <img
+                className={menuBarStyles.unconnectedIcon}
+                src={genIcon}
+                alt=""
+            />
+            <span
+                className={menuBarStyles.collapsibleLabel}
+                style={{ minWidth: "110px" }}
+            >
                 <FormattedMessage
                     defaultMessage="Generator"
                     description="Text for menubar Generator button"
                     id="gui.menuBar.Generator"
-                />: {typeName}
+                />
+                : Python
             </span>
-            <img src={dropdownCaret} alt="" />
-            <MenuBarMenu
-                className={menuBarStyles.menuBarMenu}
-                open={genMenuOpen}
-                place={isRtl ? 'left' : 'right'}
-                onRequestClose={onRequestCloseGen}
-            >
-                <MenuSection>
-                    {
-                        genList.map((item, index) => {
-                            return (
-                                <MenuItem onClick={() => changeCodeName(item)} key={index}>
-                                    <img
-                                        className={classNames(styles.check, {
-                                            [styles.selected]: generatorName === item.generatorName
-                                        })}
-                                        src={check}
-                                        alt=""
-                                    />
-                                    <span className={styles.dropdownLabel}>{item.typeName}</span>
-                                </MenuItem>
-                            )
-                        })
-                    }
-                </MenuSection>
-            </MenuBarMenu>
         </div>
-    )
+    );
 }
 
 export default React.memo(GeneratorsMenu);
