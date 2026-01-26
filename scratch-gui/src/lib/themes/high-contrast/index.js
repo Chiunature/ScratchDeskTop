c72eea7f92d81280fa447cc936effed32ebd3961
Chiunature <1540090646@qq.com>
2026-01-26 11:32:20 +0800

    删除扩展

--- scratch-gui/src/lib/themes/high-contrast/index.js
+++ scratch-gui/src/lib/themes/high-contrast/index.js
@@ -1,103 +1,97 @@
-import musicIcon from './extensions/musicIcon.svg';
-import penIcon from './extensions/penIcon.svg';
-import text2speechIcon from './extensions/text2speechIcon.svg';
-import translateIcon from './extensions/translateIcon.svg';
-import videoSensingIcon from './extensions/videoSensingIcon.svg';
+import musicIcon from "./extensions/musicIcon.svg";
+import penIcon from "./extensions/penIcon.svg";
+import text2speechIcon from "./extensions/text2speechIcon.svg";
+import translateIcon from "./extensions/translateIcon.svg";
+import videoSensingIcon from "./extensions/videoSensingIcon.svg";
 
 const blockColors = {
     // SVG colours: these must be specificed in #RRGGBB style
     // To add an opacity, this must be specified as a separate property (for SVG fill-opacity)
     motion: {
-        "primary": "#64c7f0",
-        "secondary": "#4280D7",
-        "tertiary": "#00b0f9",
-        "quaternary": "#00b0f9"
+        primary: "#64c7f0",
+        secondary: "#4280D7",
+        tertiary: "#00b0f9",
+        quaternary: "#00b0f9",
     },
     motor: {
-        "primary": "#64c7f0",
-        "secondary": "#00b0f9",
-        "tertiary": "#00b0f9",
-        "quaternary": "#00b0f9"
+        primary: "#64c7f0",
+        secondary: "#00b0f9",
+        tertiary: "#00b0f9",
+        quaternary: "#00b0f9",
     },
     combined_motor: {
-        "primary": "#2388ca",
-        "secondary": "#3373CC",
-        "tertiary": "#3373CC",
-        "quaternary": "#3373CC"
+        primary: "#2388ca",
+        secondary: "#3373CC",
+        tertiary: "#3373CC",
+        quaternary: "#3373CC",
     },
     matrix: {
-        "primary": "#7986b8",
-        "secondary": "#6877b3",
-        "tertiary": "#6877b3",
-        "quaternary": "#6877b3"
+        primary: "#7986b8",
+        secondary: "#6877b3",
+        tertiary: "#6877b3",
+        quaternary: "#6877b3",
     },
     looks: {
-        "primary": "#9966FF",
-        "secondary": "#855CD6",
-        "tertiary": "#774DCB",
-        "quaternary": "#774DCB"
+        primary: "#9966FF",
+        secondary: "#855CD6",
+        tertiary: "#774DCB",
+        quaternary: "#774DCB",
     },
     sounds: {
-        "primary": "#a981a4",
-        "secondary": "#8f6e8a",
-        "tertiary": "#8f6e8a",
-        "quaternary": "#8f6e8a"
+        primary: "#a981a4",
+        secondary: "#8f6e8a",
+        tertiary: "#8f6e8a",
+        quaternary: "#8f6e8a",
     },
     control: {
-        "primary": "#e76564",
-        "secondary": "#EC9C13",
-        "tertiary": "#cf5959",
-        "quaternary": "#cf5959"
+        primary: "#e76564",
+        secondary: "#EC9C13",
+        tertiary: "#cf5959",
+        quaternary: "#cf5959",
     },
     event: {
-        "primary": "#cd758f",
-        "secondary": "#be637f",
-        "tertiary": "#be637f",
-        "quaternary": "#be637f"
+        primary: "#cd758f",
+        secondary: "#be637f",
+        tertiary: "#be637f",
+        quaternary: "#be637f",
     },
     sensing: {
-        "primary": "#ecc0ab",
-        "secondary": "#eca888",
-        "tertiary": "#eca888",
-        "quaternary": "#eca888"
-    },
-    sensing_camera: {
-        "primary": "#0fbd8c",
-        "secondary": "#0fbd8c",
-        "tertiary": "#0fbd8c",
-        "quaternary": "#0fbd8c"
+        primary: "#ecc0ab",
+        secondary: "#eca888",
+        tertiary: "#eca888",
+        quaternary: "#eca888",
     },
     pen: {
-        "primary": "#0fBD8C",
-        "secondary": "#0DA57A",
-        "tertiary": "#0B8E69",
-        "quaternary": "#0B8E69"
+        primary: "#0fBD8C",
+        secondary: "#0DA57A",
+        tertiary: "#0B8E69",
+        quaternary: "#0B8E69",
     },
     operators: {
-        "primary": "#f5b44a",
-        "secondary": "#46B946",
-        "tertiary": "#e49e2f",
-        "quaternary": "#e49e2f"
+        primary: "#f5b44a",
+        secondary: "#46B946",
+        tertiary: "#e49e2f",
+        quaternary: "#e49e2f",
     },
     data: {
-        "primary": "#a5c9e9",
-        "secondary": "#84ace9",
-        "tertiary": "#84ace9",
-        "quaternary": "#84ace9"
+        primary: "#a5c9e9",
+        secondary: "#84ace9",
+        tertiary: "#84ace9",
+        quaternary: "#84ace9",
     },
     // This is not a new category, but rather for differentiation
     // between lists and scalar variables.
     data_lists: {
-        "primary": "#a5c9e9",
-        "secondary": "#84ace9",
-        "tertiary": "#84ace9",
-        "quaternary": "#84ace9"
+        primary: "#a5c9e9",
+        secondary: "#84ace9",
+        tertiary: "#84ace9",
+        quaternary: "#84ace9",
     },
     more: {
-        "primary": "#82be97",
-        "secondary": "#379f5a",
-        "tertiary": "#379f5a",
-        "quaternary": "#82be97"
+        primary: "#82be97",
+        secondary: "#379f5a",
+        tertiary: "#379f5a",
+        quaternary: "#82be97",
     },
     text: "#575E75",
     workspace: "#F9F9F9",
@@ -107,7 +101,7 @@ const blockColors = {
     toolbox: "#FFFFFF",
     flyout: "#F9F9F9",
     scrollbar: "#CECDCE",
-    scrollbarHover: '#CECDCE',
+    scrollbarHover: "#CECDCE",
     textField: "#FFFFFF",
     insertionMarker: "#000000",
     insertionMarkerOpacity: 0.2,
@@ -130,28 +124,25 @@ const blockColors = {
     valueReportBorder: "#AAAAAA",
     menuHover: "rgba(0, 0, 0, 0.2)",
     themeColor: "#7986b8",
-    modalColor: "hsla(227.62, 29.73%, 59.8%, .7)"
+    modalColor: "hsla(227.62, 29.73%, 59.8%, .7)",
 };
 
 const extensions = {
     music: {
-        blockIconURI: musicIcon
+        blockIconURI: musicIcon,
     },
     pen: {
-        blockIconURI: penIcon
+        blockIconURI: penIcon,
     },
     text2speech: {
-        blockIconURI: text2speechIcon
+        blockIconURI: text2speechIcon,
     },
     translate: {
-        blockIconURI: translateIcon
+        blockIconURI: translateIcon,
     },
     videoSensing: {
-        blockIconURI: videoSensingIcon
-    }
+        blockIconURI: videoSensingIcon,
+    },
 };
 
-export {
-    blockColors,
-    extensions
-};
+export { blockColors, extensions };
